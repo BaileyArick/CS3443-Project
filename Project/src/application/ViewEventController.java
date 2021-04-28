1d0ce095784da884f04e98a25f0f76e97bb31e01
Kingsmens99 <66391690+Kingsmens99@users.noreply.github.com>
2021-04-27 23:20:56 -0500

    Final Update 4 realz
    adadafds

--- Project/src/application/ViewEventController.java
+++ Project/src/application/ViewEventController.java
@@ -8,10 +8,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import application.model.AppModel;
@@ -89,7 +91,11 @@ public class ViewEventController {
 		
 		if(keys.size() == 0)
 		{
-			System.out.println("No matches");
+			Alert error = new Alert(AlertType.ERROR);
+			error.setTitle("Error Message");
+			error.setHeaderText("No Event Found");
+			error.setContentText("Sorry, that event could not be located.");	
+			error.show();
 		}
 		else
 		{
